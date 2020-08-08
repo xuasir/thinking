52364861ff2d7258b32351b31678ba471eaa46dc
xuguo <xuguo@leimingtech.com>
2020-08-08 09:15:07 +0800

    build(utils): 为utils添加打包字段

--- packages/toolbox/index.js
+++ packages/toolbox/index.js
@@ -1,1 +1,6 @@
-module.exports = require('dist/toolbox.cjs.js')
+'use strict'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./dist/toolbox.cjs.prod.js')
+} else {
+  module.exports = require('./dist/toolbox.cjs.js')
+}
