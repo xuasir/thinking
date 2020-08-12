f7e8d9363f6c95daafe490531b8039c44723c3e5
xuguo <xuguo@leimingtech.com>
2020-08-12 16:18:16 +0800

    refactor(usedebounce): 优化usedebounce避免卸载时未释放

--- packages/vuehooks/src/useDebounceRef/index.ts
+++ packages/vuehooks/src/useDebounceRef/index.ts
@@ -10,13 +10,16 @@ import { useDebounceFn } from '../useDebounceFn'
 
 export function useDebounceRef<T extends Ref>(rawValue: T, wait: number): T {
   const debounceValue = ref<UnwrapRef<T>>(rawValue.value)
-  const { run } = useDebounceFn<UnwrapRef<T>[]>((newValue) => {
+  const { run, cancel } = useDebounceFn<UnwrapRef<T>[]>((newValue) => {
     debounceValue.value = newValue
   }, wait)
   const stop = watch(rawValue, (newValue) => run(newValue))
 
   if (getCurrentInstance()) {
-    onUnmounted(() => stop())
+    onUnmounted(() => {
+      stop()
+      cancel()
+    })
   }
 
   return debounceValue
