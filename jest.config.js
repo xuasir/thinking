8d74a0a55ad6643ed14028f3ce453d35d0cf2ccf
xuguo <xuguo@leimingtech.com>
2020-08-17 10:23:26 +0800

    test(jest): 采用vue2.x测试hooks

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ const path = require('path')
 module.exports = {
   preset: 'ts-jest',
   rootDir: __dirname,
+  globals: {
+    __VUE2__: false,
+  },
   collectCoverage: true,
   collectCoverageFrom: ['**/__test__/**'],
   coverageDirectory: path.resolve(__dirname, 'coverage'),
@@ -10,5 +13,8 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   watchPathIgnorePatterns: ['node_modules'],
   testMatch: ['<rootDir>/packages/**/__test__/**/*spec.[jt]s?(x)'],
-  setupFiles: ['<rootDir>/packages/vuehooks/__test__/setupTest.js'],
+  setupFiles: [
+    '<rootDir>/packages/vuehooks/__test__/helper/useComposition.ts',
+    '<rootDir>/packages/vuehooks/__test__/setupTest.js',
+  ],
 }
