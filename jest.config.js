3f716c25e3c97b48577224873eaedfe970fbc114
xuguo <xuguo@leimingtech.com>
2020-08-21 18:02:40 +0800

    chore: 移除vue-hooks子包,已独立出去

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,6 @@ const path = require('path')
 module.exports = {
   preset: 'ts-jest',
   rootDir: __dirname,
-  globals: {
-    __VUE2__: false,
-  },
   collectCoverage: true,
   collectCoverageFrom: ['**/__test__/**'],
   coverageDirectory: path.resolve(__dirname, 'coverage'),
@@ -13,8 +10,4 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   watchPathIgnorePatterns: ['node_modules'],
   testMatch: ['<rootDir>/packages/**/__test__/**/*spec.[jt]s?(x)'],
-  setupFiles: [
-    '<rootDir>/packages/vuehooks/__test__/helper/useComposition.ts',
-    '<rootDir>/packages/vuehooks/__test__/setupTest.js',
-  ],
 }
--- packages/vuehooks/src/useDrop/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './useDrag'
-export * from './useDrop'
