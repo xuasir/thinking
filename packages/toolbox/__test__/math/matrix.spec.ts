86963df24d61a262b494c8b99633daa42a186fe0
xuguo <xuguo@leimingtech.com>
2020-08-21 17:05:30 +0800

    fix: 修复matrix转换行列不相等时的错误

--- packages/toolbox/__test__/math/matrix.spec.ts
+++ packages/toolbox/__test__/math/matrix.spec.ts
@@ -16,4 +16,20 @@ describe('test math method ', () => {
     expect(m.getRawMatrix()).toEqual(rawMatrix)
     expect(m.transpose()).toEqual(transposeMatrix)
   })
+
+  const rawData = [
+    ['c1', 'c2'],
+    ['c1', 'c2'],
+    ['c1', 'c2'],
+  ]
+  const transposeData = [
+    ['c1', 'c1', 'c1'],
+    ['c2', 'c2', 'c2'],
+  ]
+
+  test('test transpose', () => {
+    const m = new Matrix(rawData)
+    expect(m.getRawMatrix()).toEqual(rawData)
+    expect(m.transpose()).toEqual(transposeData)
+  })
 })
