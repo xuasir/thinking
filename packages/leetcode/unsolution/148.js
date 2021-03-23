e45e9157fce50bc9de1852ede6242343a6212f8f
guo.xu <xuguo@outlook.it>
2021-03-23 10:13:14 +0800

    chore: leetcode独立管理

--- packages/leetcode/unsolution/148.js
+++
@@ -1,5 +0,0 @@
-// https://leetcode-cn.com/problems/sort-list/
-
-// 自底向上的归并排序 排序链表
-
-var sortList = function (head) {}
--- packages/leetcode/unsolution/394.js
+++
@@ -1,8 +0,0 @@
-// problem: https://leetcode-cn.com/problems/decode-string/
-var decodeString = function (s) {
-  let len = s.length
-  if (len < 1) return ''
-  let stack = [],
-    i = 0
-  while (i <= len) {}
-}
